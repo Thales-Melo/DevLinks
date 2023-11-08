09ceae9eafbb63e98bb65a4a5aaa7968ee36b615
Thales Melo <thales.penha@edu.ufes.br>
2023-11-08 03:32:14 -0300

    DONE

---
+++ script.js
@@ -0,0 +1,18 @@
+// Thales Melo da Penha
+
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    // O seguite trecho serve para alterar a foto de perfil de acordo com o tema
+    // Mas quero utilizar somente uma foto mesmo
+
+    // const img = document.querySelector("#profile img")
+
+    // if (html.classList.contains("light")) {
+    //     img.setAttribute("src", "assets/IMAGEM")
+    // }
+    // else {
+    //     img.setAttribute("src", "assets/IMAGEM_2")
+    // }
+}
